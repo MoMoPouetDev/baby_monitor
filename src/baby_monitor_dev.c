b97a6c450071a9aaded56cc9f3b20786036b7bb6
morgan <morgan@debian>
2018-06-04 13:39:55 +0200

    init gtk

---
+++ src/baby_monitor_dev.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : baby_monitor_dev.c
+ Author      : morgan venandy
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk-2.0/gtk/gtk.h>
+
+int main(int argc, char **argv) {
+
+	GtkWidget* MainWindow = NULL;
+
+	gtk_init(&argc, &argv);
+
+	MainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(MainWindow), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+
+	/* Affichage et boucle évènementielle */
+	gtk_widget_show(MainWindow);
+	gtk_main();
+
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
