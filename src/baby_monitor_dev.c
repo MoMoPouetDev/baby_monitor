e122296eedbb53595479d449f7e13a2fa805ec58
morgan <morgan@debian>
2018-06-09 17:21:47 +0200

    manage button

--- src/baby_monitor_dev.c
+++ src/baby_monitor_dev.c
@@ -30,6 +30,28 @@ void playerOnRealize(GtkWidget *widget, gpointer data)
     libvlc_media_player_set_xwindow(MediaPlayer, GDK_WINDOW_XID(gtk_widget_get_window(widget)));
 }
 
+void onClickMute(GtkWidget *widget, gpointer data)
+{
+	gboolean EtatButton;
+	EtatButton = gtk_toggle_button_get_mode(GTK_TOGGLE_BUTTON(widget));
+	gtk_toggle_button_set_mode(GTK_TOGGLE_BUTTON(widget), (EtatButton ^ TRUE));
+	if(!EtatButton) {
+		gtk_button_set_label(GTK_BUTTON(widget), "Unmute");
+	}
+	else {
+		gtk_button_set_label(GTK_BUTTON(widget), "Mute");
+	}
+}
+void onClickUp(GtkWidget *widget, gpointer data)
+{
+	//gtk_image_set_from_file();
+}
+
+void onClickDown(GtkWidget *widget, gpointer data)
+{
+
+}
+
 int main(int argc, char **argv)
 {
     /* Variables */
@@ -40,7 +62,9 @@ int main(int argc, char **argv)
     			*MainTable = NULL,
     			*ButtonSound[3],
     			*ButtonPower = NULL,
-				*VideoWindow = NULL;
+				*VideoWindow = NULL,
+				*VUImage[10],
+				*VolumeImage[10];
 
     sigset_t set;
     signal(SIGCHLD, SIG_DFL);
@@ -58,20 +82,38 @@ int main(int argc, char **argv)
     gtk_window_set_default_size(GTK_WINDOW(MainWindow), 480, 320);
 
     /* Création des tables */
-    MainTable = gtk_table_new(8, 10, TRUE);
+    MainTable = gtk_table_new(20, 20, TRUE);
     gtk_container_add(GTK_CONTAINER(MainWindow), GTK_WIDGET(MainTable));
 
     /* Création des boutons */
     ButtonSound[0] = gtk_button_new_with_label("+");
-    ButtonSound[1] = gtk_button_new_with_label("Mute");
+    ButtonSound[1] = gtk_toggle_button_new_with_label("Mute");
     ButtonSound[2] = gtk_button_new_with_label("-");
 
     ButtonPower = gtk_button_new_with_label("OFF");
 
+    int LeftAttach = 0;
+    int RightAttach = 0;
+    /* Création des Images */
+    for(int i=0; i<10; i++) {
+    	VUImage[i] = gtk_image_new_from_file("/home/morgan/testImage.png");
+    	LeftAttach = 2+i;
+    	RightAttach = 3+i;
+    	gtk_table_attach_defaults(GTK_TABLE(MainTable), VUImage[i], LeftAttach, RightAttach, 17, 19);
+    }
+
+    int TopAttach = 0;
+    int BottomAttach = 0;
+    for(int i=0; i<10; i++){
+    	VolumeImage[i] = gtk_image_new_from_file("/home/morgan/testImageVolume.png");
+    	TopAttach = 3+i;
+		BottomAttach = 4+i;
+		gtk_table_attach_defaults(GTK_TABLE(MainTable), VolumeImage[i], 18, 19, TopAttach, BottomAttach);
+    }
+
     /* Insertion VLC */
     VideoWindow = gtk_drawing_area_new();
-    gtk_table_attach_defaults(GTK_TABLE(MainTable), VideoWindow, 1, 6, 1, 6);
-    //gtk_container_add(GTK_CONTAINER(MainWindow), VideoWindow);
+    gtk_table_attach_defaults(GTK_TABLE(MainTable), VideoWindow, 1, 17, 1, 13);
 
     VlcInst = libvlc_new(0, NULL);
     MediaPlayer = libvlc_media_player_new(VlcInst);
@@ -83,16 +125,17 @@ int main(int argc, char **argv)
 	//g_free(VideoURI);
 
     /* Insertion des boutons */
-    gtk_table_attach(GTK_TABLE(MainTable), ButtonPower, 8, 9, 1, 2, GTK_EXPAND | GTK_FILL, GTK_EXPAND, 0, 0);
-    gtk_table_attach(GTK_TABLE(MainTable), ButtonSound[0], 8, 9, 3, 4, GTK_EXPAND | GTK_FILL, GTK_EXPAND, 0, 0);
-    gtk_table_attach(GTK_TABLE(MainTable), ButtonSound[1], 8, 9, 4, 5, GTK_EXPAND | GTK_FILL, GTK_EXPAND, 0, 0);
-    gtk_table_attach(GTK_TABLE(MainTable), ButtonSound[2], 8, 9, 5, 6, GTK_EXPAND | GTK_FILL, GTK_EXPAND, 0, 0);
-
+    gtk_table_attach_defaults(GTK_TABLE(MainTable), ButtonPower, 18, 19, 1, 2);
+    gtk_table_attach_defaults(GTK_TABLE(MainTable), ButtonSound[0], 18, 19, 13, 15);
+    gtk_table_attach_defaults(GTK_TABLE(MainTable), ButtonSound[1], 18, 19, 15, 17);
+    gtk_table_attach_defaults(GTK_TABLE(MainTable), ButtonSound[2], 18, 19, 17, 19);
 
     /* Création des signaux */
     g_signal_connect(G_OBJECT(MainWindow), "destroy", G_CALLBACK(onDestroy), NULL);
+    g_signal_connect(G_OBJECT(ButtonSound[1]), "clicked", G_CALLBACK(onClickMute), ButtonSound[1]);
+    g_signal_connect(G_OBJECT(ButtonSound[0]), "clicked", G_CALLBACK(onClickUp), VolumeImage);
+    g_signal_connect(G_OBJECT(ButtonSound[2]), "clicked", G_CALLBACK(onClickDown), VolumeImage);
 
-    /* Intégration VLC */
 
     /* Affichage et boucle événementielle */
     gtk_widget_show_all(MainWindow);
